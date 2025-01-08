a9f03ed566eed1ed9c660858e158a02f9ae66eb6
verdantpath <m_dittmann@yahoo.com>
2025-01-07 19:50:36 -0800

    Add about page

---
+++ portfolio/app/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    This page is about me
+  </div>
+  <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div>
+</template>
+<script setup>
+// definePageMeta({
+//   layout: "another",
+// })
+function enableCustomLayout() {
+  setPageLayout('another')
+}
+definePageMeta({
+  layout: false,
+});
+</script>
+<style>
+
+</style>
