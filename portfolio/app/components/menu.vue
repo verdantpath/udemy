a9a3efda1c1c63d20ac2348295d34c95146ffe8a
verdantpath <m_dittmann@yahoo.com>
2025-01-10 19:23:08 -0800

    Add menu component

---
+++ portfolio/app/components/menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="font-mono">
+    <ul class="flex space-x-4">
+      <li>
+        <NuxtLink to="/" class="link">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style scoped>
+.link {
+  @apply p-1 hover:bg-gray-200
+}
+</style>
