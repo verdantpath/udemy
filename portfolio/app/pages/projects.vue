c7a4311a31bd132c25668a60d15622f0ab993744
verdantpath <m_dittmann@yahoo.com>
2025-01-14 20:02:33 -0800

    Add projects page

---
+++ portfolio/app/pages/projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-semibold mb-10">Projects</h2>
+    <ProjectList />
+  </div>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style>
+
+</style>
