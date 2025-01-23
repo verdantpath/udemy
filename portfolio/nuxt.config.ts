8483edee8a755167d6a7dd0c3b455faf20e4701a
verdantpath <m_dittmann@yahoo.com>
2025-01-22 17:38:40 -0800

    Update nuxt config

---
+++ portfolio/nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }, 
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/content'
+  ],
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark'
+      }
+    }
+  },
+  colorMode: {
+    classSuffix: ''
+  }
+})
