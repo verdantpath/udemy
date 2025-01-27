4f1c7b2acd7bb5155422ca61131b478f1839cf93
verdantpath <m_dittmann@yahoo.com>
2025-01-26 23:27:32 -0800

    Add blog slug

---
+++ portfolio/app/pages/blog/[...slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
+    <ContentDoc />
+  </article>
+</template>
+<script setup>
+const route = useRoute()
+console.log(route.params.slug) 
+</script>
+<style>
+
+</style>
