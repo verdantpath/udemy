0541f37312f4fa88407b0eb7bcee29ea9efd2b9d
verdantpath <m_dittmann@yahoo.com>
2025-01-29 19:08:34 -0800

    Add project list

---
+++ portfolio/app/components/project-list.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="mb-10">Projects</p>
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Something went wrong... Try again!</section>
+  <section v-else>Here we display the repos!
+    <!-- <div><pre>{{ data }}</pre></div> -->
+    <ul class="grid grid-cols-1 gap-4">
+      <li v-for="repository in repos" :key="repository.id" class="border border-gray-200 rounded-ms p-4 hover:bg-gray-100 font-mono">
+        <a :href="repository.html_url" target="_blank">
+          <div class="flex items-center justify-between text-sm">
+            <div class="font-semibold">{{  repository.name  }}</div>
+            <div>{{ repository.stargazers_count }} ★</div>
+          </div>
+          <p class="text-sm">
+            {{ repository.description }}
+          </p>
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
+<script setup>
+// const { error, pending, data } = await useFetch(
+//   'https://api.github.com/users/verdantpath/repos'
+//   )
+const { error, pending, data } = await useFetch(
+  'https://api.github.com/users/piotr-jura-udemy/repos'
+)
+const repos = computed(() => data.value.filter(repo => repo.description).sort((a, b) => b.stargazers_count - a.stargazers_count))
+</script>
+<style>
+
+</style>
