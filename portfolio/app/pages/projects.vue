3305275da13714c3ee60466d8cad24deb03b7064
verdantpath <m_dittmann@yahoo.com>
2025-02-03 18:05:23 -0800

    Update projects page

--- portfolio/app/pages/projects.vue
+++ portfolio/app/pages/projects.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <h2 class="text-2xl font-semibold mb-10">Projects</h2>
-    <ProjectList />
-  </div>
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc path="/projects" />
+  </article>
 </template>
-<script>
-  export default {
-    
-  }
-</script>
-<style>
-
-</style>
