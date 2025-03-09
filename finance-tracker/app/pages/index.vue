2decdcbc2b5ce8b075b763b2a259dbae2bce51f9
verdantpath <m_dittmann@yahoo.com>
2025-03-09 11:27:59 -0700

    Update index page

--- finance-tracker/app/pages/index.vue
+++ finance-tracker/app/pages/index.vue
@@ -8,17 +8,20 @@
     </div>
   </section>
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="false" />
-    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false" />
-    <Trend color="red" title="Saving" :amount="4000" :last-amount="5000" :loading="false" />
+    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Saving" :amount="4000" :last-amount="5000" :loading="isLoading" />
   </section>
 
-  <section>
+  <section v-if="!isLoading">
     <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
       <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
-      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" />
+      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" @deleted="refreshTransactions()" />
     </div>
+  </section>
+  <section v-else>
+    <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
   </section>
 </template>
 <script setup>
@@ -29,6 +32,9 @@ const supabase = useSupabaseClient()
 
 const transactions = ref([])
 const isLoading = ref(false) 
+
+const income = computed(() => transactions.value.filter(t => t.type === 'Income') )
+const expense = computed(() => transactions.value.filter(t => t.type === 'Expense') )
 
 const fetchTransactions = async () => {
   isLoading.value = true
@@ -51,7 +57,9 @@ const fetchTransactions = async () => {
 
 // console.log(data)
 
-transactions.value = await fetchTransactions()
+const refreshTransactions = async () => transactions.value = await fetchTransactions()
+
+await refreshTransactions()
 
 const transactionsGroupedByDate = computed(() => {
   let grouped = {}
