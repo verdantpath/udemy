a50aa7ff5542b00f2065e513cae8027e690556fb
verdantpath <m_dittmann@yahoo.com>
2025-03-28 20:48:27 -0700

    Update index page

--- finance-tracker/app/pages/index.vue
+++ finance-tracker/app/pages/index.vue
@@ -8,10 +8,10 @@
     </div>
   </section>
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="5000" :loading="isLoading" />
-    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Saving" :amount="4000" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="prevIncomeTotal" :loading="pending" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="prevExpenseTotal" :loading="pending" />
+    <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="pending" />
+    <Trend color="red" title="Saving" :amount="4000" :last-amount="5000" :loading="pending" />
   </section>
 
   <section class="flex justify-between mb-10">
@@ -22,15 +22,15 @@
       </div>
     </div>
     <div>
-      <TransactionModal v-model="isOpen" @saved="refreshTransactions()" />
+      <TransactionModal v-model="isOpen" @saved="refresh()" />
       <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
     </div>
   </section>
 
-  <section v-if="!isLoading">
-    <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
+  <section v-if="!pending">
+    <div v-for="(transactionsOnDay, date) in byDate" :key="date" class="mb-10">
       <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
-      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" @deleted="refreshTransactions()" />
+      <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" @deleted="refresh()" />
     </div>
   </section>
   <section v-else>
@@ -41,78 +41,23 @@
 import { transactionViewOptions } from '~/constants'
 const selectedView = ref(transactionViewOptions[1])
 
-const supabase = useSupabaseClient()
-
-const transactions = ref([])
-const isLoading = ref(false) 
 const isOpen = ref(false)
 
-const income = computed(() => transactions.value.filter(t => t.type === 'Income') )
-const expense = computed(() => transactions.value.filter(t => t.type === 'Expense') )
+const { current, previous } = useSelectedTimePeriod(selectedView)
 
-const incomeCount = computed(() => income.value.length)
-const expenseCount = computed(() => expense.value.length) 
-
-const incomeTotal = computed(() => {
-  return income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-})
-const expenseTotal = computed(() => {
-  return expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
-})
-
-
-const fetchTransactions = async () => {
-  isLoading.value = true
-  try {
-    const { data } = await useAsyncData('transactions', async() => {
-      const { data, error } = await supabase
-        .from('transactions')
-        .select()
-        .order('created_at', { ascending: false })
-
-      if (error) return []
-
-      return data
-    })
-
-    return data.value
-  } finally {
-    isLoading.value = false
+const { pending, refresh, transactions: { 
+  incomeCount,
+  expenseCount,
+  incomeTotal,
+  expenseTotal,
+  grouped: {
+    byDate
   }
-}
+}} = useFetchTransactions(current)
 
-// console.log(data)
+const { transactions: { 
+  incomeTotal: prevIncomeTotal,
+  expenseTotal: prevExpenseTotal,
+}} = useFetchTransactions(previous)
 
-const refreshTransactions = async () => transactions.value = await fetchTransactions()
-
-await refreshTransactions()
-
-const transactionsGroupedByDate = computed(() => {
-  let grouped = {}
-
-  for (const transaction of transactions.value) {
-    const date = new Date(transaction.created_at).toISOString().split('T')[0]
-
-    if (!grouped[date]) {
-      grouped[date] = []
-    }
-    grouped[date].push(transaction)
-    //console.log('@@ grouped ', grouped)
-  }
-  // const sortedKeys = Object.keys(grouped).sort().reverse()
-  // const sortedGrouped = {}
-
-  // for (const key of sortedKeys) {
-  //   sortedGrouped[key] = grouped[key]
-  // }
-  // return sortedGrouped
-
-  return grouped
-})
-// console.log('date ', date);
-// console.log('tgbd ',transactionsGroupedByDate.value);
 </script>
-
-<style>
-
-</style>
