58a1226ee2993c3b71c1cc8c7adf5f788f4943fb
verdantpath <m_dittmann@yahoo.com>
2025-04-05 12:06:37 -0700

    Update login page

--- finance-tracker/app/pages/login.vue
+++ finance-tracker/app/pages/login.vue
@@ -27,6 +27,8 @@ const pending = ref(false)
 const toast = useToast()
 const supabase = useSupabaseClient()
 
+useRedirectIfAuthenticated()
+
 const handleLogin = async () => {
   pending.value = true
 
@@ -34,7 +36,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: 'http://localhost:3000'
+        emailRedirectTo: 'http://localhost:3000/confirm'
       }
     })
 
