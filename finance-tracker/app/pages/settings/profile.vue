6a20f164ee4b0a33fa9a0a1f282072712b15e85a
verdantpath <m_dittmann@yahoo.com>
2025-04-24 10:21:10 -0700

    Update profile settings page

--- finance-tracker/app/pages/settings/profile.vue
+++ finance-tracker/app/pages/settings/profile.vue
@@ -1,12 +1,12 @@
 <template>
-<UForm :state="state" :schema="schema">
+<UForm :state="state" :schema="schema" @submit.prevent="saveProfile">
   <UFormGroup class="mb-4" label="Full Name" name="name">
     <UInput v-model="state.name" />
   </UFormGroup>
   <UFormGroup class="mb-4" label="Email" name="email" help="You will receive a confirmation email">
     <UInput v-model="state.email" />
   </UFormGroup>
-  <UButton type="submit" color="black" variant="solid" />
+  <UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
 </UForm>
 </template>
 <script setup>
@@ -19,7 +19,7 @@
   const pending = ref(false)
 
   const state = ref({
-    name: '',
+    name: user.value.user_metadata?.full_name,
     email: user.value.email
   })
 
@@ -27,5 +27,37 @@
     name: z.string().min(2).optional(),
     email: z.string().email() 
   })
+
+  const saveProfile = async () => {
+    pending.value = true
+
+    try {
+      const data = {
+        data: {
+          full_name: state.value.name
+        }
+      }
+
+      if (state.value.email !== user.value.email) {
+        data.email = state.value.email
+      }
+
+      console.log(data)
+      const { error } = await supabase.auth.updateUser(data)
+      if (error) throw error
+
+      toastSuccess({
+        title: 'Profile updated',
+        description: 'Your profile has been updated'
+      })
+    } catch (error) {
+      toastError({
+        title: 'Error updating profile',
+        description: error.message
+      })
+    } finally {
+      pending.value = false
+    }
+  }
 
 </script>
