cce22a31dd0631fc119b60ff5eb1de853bb2cc89
verdantpath <m_dittmann@yahoo.com>
2025-04-26 12:36:23 -0700

    Update index page

--- finance-tracker/app/pages/index.vue
+++ finance-tracker/app/pages/index.vue
@@ -38,8 +38,14 @@
   </section>
 </template>
 <script setup>
+
+
+
 import { transactionViewOptions } from '~/constants'
-const selectedView = ref(transactionViewOptions[1])
+
+const user = useSupabaseUser()
+
+const selectedView = ref(user.value.user_metadata?.transaction_view ?? transactionViewOptions[1])
 
 const isOpen = ref(false)
 
