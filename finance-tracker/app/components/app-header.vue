2f01fc3e4c006e663471587eb9b19fb6678e05a2
verdantpath <m_dittmann@yahoo.com>
2025-04-29 17:31:52 -0700

    Update app-header

--- finance-tracker/app/components/app-header.vue
+++ finance-tracker/app/components/app-header.vue
@@ -12,7 +12,7 @@
                 Signed in as
               </p>
               <p class="font-medium text-gray-900 dark:text-white">
-                {{ item.label }}
+                {{ user.email }}
               </p>
             </div>
           </template>
@@ -30,13 +30,12 @@ const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 const items = [
   [{
-    label: user.value?.email,
     slot: 'account',
     disabled: true
   }], [{
     label: 'Settings',
     icon: 'i-heroicons-cog-8-tooth',
-    click: () => console.log('Link to settings in the future')
+    click: () => navigateTo('/settings/profile')
   }, {
     label: 'Sign out',
     icon: 'i-heroicons-arrow-left-on-rectangle',
