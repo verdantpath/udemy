0f62339e841f6422397b5bc04e622e94e91efbf1
verdantpath <m_dittmann@yahoo.com>
2025-05-21 19:44:04 -0700

    Update header

--- finance-tracker/app/components/app-header.vue
+++ finance-tracker/app/components/app-header.vue
@@ -5,7 +5,7 @@
     </NuxtLink>
     <div>
       <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }" v-if="user">
-        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+        <UAvatar :src="url" alt="Avatar" />
           <template #account="{ item }">
             <div class="text-left">
               <p>
@@ -28,6 +28,7 @@
 <script setup>
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
+const { url } = useAvatarUrl()
 const items = [
   [{
     slot: 'account',
