85d73adfbf7d576e66093fb68c9701a5f97e714d
verdantpath <m_dittmann@yahoo.com>
2025-05-23 20:01:28 -0700

    Update transaction modal

--- finance-tracker/app/components/transaction-modal.vue
+++ finance-tracker/app/components/transaction-modal.vue
@@ -2,11 +2,11 @@
   <UModal v-model="isOpen">
     <UCard>
       <template #header>
-        Add Transaction
+        {{ isEditing ? "Edit" : "Add" }} Transaction
       </template>
       <UForm :state="state" :schema="schema" ref="form" @submit="save">
         <UFormGroup :required="true" label="Transaction Type" name="type" class="mb-4">
-          <USelect placeholder="Select the Transaction Type" :options="types" v-model="state.type" />
+          <USelect :disabled="isEditing" placeholder="Select the Transaction Type" :options="types" v-model="state.type" />
         </UFormGroup>
         <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
           <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
@@ -30,8 +30,14 @@ import { categories, types } from '~/constants'
 import { z } from 'zod'
 
 const props = defineProps({
-  modelValue: Boolean
+  modelValue: Boolean,
+  transaction: {
+    type: Object,
+    required: false
+  }
 })
+const isEditing = computed(() => !!props.transaction)
+
 const emit = defineEmits(['update:modelValue', 'saved'])
 
 const defaultSchema = z.object({
@@ -70,7 +76,10 @@ const save = async () => {
   isLoading.value = true 
   try {
     const { error } = await supabase.from('transactions')
-      .upsert({...state.value})
+      .upsert({
+        ...state.value,
+        id: props.transaction?.id
+      })
 
       if (!error) {
       toastSuccess({
@@ -98,9 +107,13 @@ const initialState = {
   description: undefined,
   category: undefined
 }
-const state = ref({
-  ...initialState
-})
+const state = ref(isEditing.value ? {
+  type: props.transaction.type,
+  amount: props.transaction.amount,
+  created_at: props.transaction.created_at.split('T')[0],
+  description: props.transaction.description,
+  category: props.transaction.category
+} : {...initialState})
 
 const resetForm = () => {
   Object.assign(state.value, initialState)
