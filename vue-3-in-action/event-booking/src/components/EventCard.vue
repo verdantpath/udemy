37f8eeca0fcac7f375232f4f68f91940b9315b66
verdantpath <m_dittmann@yahoo.com>
2026-04-11 13:56:05 -0700

    Add event card component

---
+++ vue-3-in-action/event-booking/src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+  </div>
+</template>
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String,
+})
+</script>
+<style></style>
