e6a82885bd696c41e7c7658516efed080666c8e0
verdantpath <m_dittmann@yahoo.com>
2026-04-27 17:17:20 -0700

    Add button component

---
+++ vue-3-in-action/event-booking/src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
+    @click="$emit('click')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+})
+</script>
