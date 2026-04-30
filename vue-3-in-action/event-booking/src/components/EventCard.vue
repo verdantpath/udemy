66acf7624ad159f0e357a4198af25851350ff162
verdantpath <m_dittmann@yahoo.com>
2026-04-29 19:20:11 -0700

    Update event card

--- vue-3-in-action/event-booking/src/components/EventCard.vue
+++ vue-3-in-action/event-booking/src/components/EventCard.vue
@@ -3,13 +3,24 @@
     <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
     <p class="p-4 border-b border-gray-200">{{ when }}</p>
     <p class="p-4">{{ description }}</p>
+    <section class="flex justify-end p-4">
+      <!-- <button
+        class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
+        @click="$emit('register')"
+      >
+        Register
+      </button> -->
+      <Button label="Register" @click="$emit('register')"></Button>
+    </section>
   </div>
 </template>
 <script setup>
+import Button from '@/components/Button.vue'
 defineProps({
   title: String,
   when: String,
   description: String,
 })
+defineEmits(['register'])
 </script>
 <style></style>
