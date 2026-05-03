ea0c1ecd5c9beff9b3515781c7783c7a4f62d333
verdantpath <m_dittmann@yahoo.com>
2026-05-02 21:27:49 -0700

    Update event card component

--- vue-3-in-action/event-booking/src/components/EventCard.vue
+++ vue-3-in-action/event-booking/src/components/EventCard.vue
@@ -1,26 +1,7 @@
 <template>
-  <div class="border border-gray-200 bg-white rounded-md">
-    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
-    <p class="p-4 border-b border-gray-200">{{ when }}</p>
-    <p class="p-4">{{ description }}</p>
-    <section class="flex justify-end p-4">
-      <!-- <button
-        class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
-        @click="$emit('register')"
-      >
-        Register
-      </button> -->
-      <Button label="Register" @click="$emit('register')"></Button>
-    </section>
-  </div>
+  <RoundedCard></RoundedCard>
 </template>
 <script setup>
-import Button from '@/components/Button.vue'
-defineProps({
-  title: String,
-  when: String,
-  description: String,
-})
-defineEmits(['register'])
+import RoundedCard from '@/components/RoundedCard.vue'
 </script>
 <style></style>
