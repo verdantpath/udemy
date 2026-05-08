91a237cc777385769ba26d4ff91bbd52f85ec3fa
verdantpath <m_dittmann@yahoo.com>
2026-05-07 21:00:01 -0700

    Add rounded card component

---
+++ vue-3-in-action/event-booking/src/components/RoundedCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+    <section class="flex justify-end p-4">
+      <RoundButton @click="$emit('register')">Register</RoundButton>
+    </section>
+  </div>
+</template>
+<script setup>
+import RoundButton from '@/components/RoundButton.vue'
+defineProps({
+  title: String,
+  when: String,
+  description: String,
+})
+defineEmits(['register'])
+</script>
+<style></style>
